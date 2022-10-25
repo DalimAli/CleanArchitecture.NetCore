bb9eb1e52f8c29092a7ee05631acf8db0b3370e4
DalimAli <dalim3281@gmail.com>
2022-10-25 21:54:41 +0600

    presentation layer implemention

---
+++ CleanArchitecture.Presentation.Mvc/Data/ApplicationDbContext2.cs
@@ -0,0 +1,15 @@
+﻿using CleanArchitecture.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitecture.Presentation.Mvc.Data
+{
+    public class ApplicationDbContext2 : DbContext
+    {
+        public ApplicationDbContext2(DbContextOptions options) : base(options)
+        {
+
+        } 
+
+        public DbSet<Course> Courses { get; set; }
+    }
+}
